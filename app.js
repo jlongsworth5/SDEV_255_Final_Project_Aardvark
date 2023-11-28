b2aafd5d47e352f5d429be55e0be3f50c0b75fa5
jlongsworth5 <jlongsworth@ivytech.edu>
2023-11-28 18:24:23 -0500

    Add routing to server

--- app.js
+++ app.js
@@ -3,3 +3,30 @@ const express = require('express');
 
 // Setup express application
 const app = express();
+
+// Listen for requests
+app.listen(3000);
+
+// Respond to requests
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.get('/courses', (req, res) => {
+    res.sendFile('./html/courses.html', { root: __dirname });
+});
+
+app.get('/staff', (req, res) => {
+    res.sendFile('./html/staff.html', { root: __dirname });
+});
+
+app.get('/students', (req, res) => {
+    res.sendFile('./html/students.html', { root: __dirname });
+});
+
+// 404 Page - When added
+/*
+app.use((req, res) => {
+    res.status(404).sendFile('./html/404.html', { root: __dirname });    
+});
+*/
