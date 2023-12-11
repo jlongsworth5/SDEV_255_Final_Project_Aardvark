5fffa58231277c9e12e1efc1a4fc6b47e2914b6f
jlongsworth5 <jlongsworth@ivytech.edu>
2023-12-10 19:51:58 -0500

    Adding CRUD routes for student course registrations

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -6,4 +6,56 @@ module.exports.student_index = (req, res) => {
     res.render('students', { title: 'Students' });
 };
 
-// Add logic to register for courses and view schedules
+module.exports.registration_create_post = (req, res) => {
+    const userId = req.params.userId;
+    const courseId = req.params.courseId;
+    const registration = new Registration(userId, courseId);
+    
+    registration.save()
+        .then((result) => {
+            res.redirect('/students');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+module.exports.registration_update_post = (req, res) => {
+    const id = req.params.id;
+    Registration.updateOne({ _id: id }, { courseId: req.body.courseId })
+        .then((result) => {
+            res.redirect('/students');
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(404).render('404', { title: '404' });
+        });
+};
+
+module.exports.registration_delete_post = (req, res) => {
+    const id = req.params.id;
+
+    Registration.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/students' });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(404).render('404', { title: '404' });
+        });
+};
+
+module.exports.registrations_get = (req, res) => {
+    let schedule = {};
+
+    const userId = req.params.userId;
+
+    try {
+        schedule = Registration.find().where('userId').equals(userId);
+        // Add logic to do something with the schedule
+    }
+    catch (err) {
+        console.log(err);
+        res.status(404).render('404', { title: '404' });
+    }
+};
